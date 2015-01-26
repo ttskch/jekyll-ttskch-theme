8e381ce85587f41c90882349696e67b45c962e31
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2015-01-26 11:46:28 +0900

    add target="blank" into all outer links automatically

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,15 @@ $(function () {
         }
     });
 
+    // add `target="_blank"` into all outer links.
+    var host = document.location.host;
+    $("a[href]").each(function() {
+        var re = new RegExp(host, "g");
+        if ($(this).attr("href").match(/\/\//) && !$(this).attr("href").match(re)) {
+            $(this).attr("target", "_blank");
+        }
+    });
+
     // social buttons.
     // @see https://github.com/creasty/minimal-sharer
     $(".minimal-sharer").each(function () {
